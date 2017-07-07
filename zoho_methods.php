c7dac81ce34151ad02e97c788c47b57aa5f450e8
xusifob <b.malahieude@free.fr>
2017-07-07 11:33:39 +0200

    update Zoho class to work with zoho eu && improve doc

--- zoho_methods.php
+++ zoho_methods.php
@@ -9,19 +9,44 @@
 
 class ZohoException extends Exception { }
 
+
+/**
+ *
+ *
+ *
+ * Class Zoho
+ */
 class Zoho {
 
-    public function __construct($authtoken, $extra_auth_params = array(), $auth_url="https://accounts.zoho.com/login") {
-	$this->authtoken = $authtoken;
+
+    protected $domain;
+
+
+
+
+    /**
+     * Zoho constructor.
+     * @param $authtoken
+     * @param array $extra_auth_params
+     * @param string $auth_url
+     * @param string $domain    The Domain for the Zoho API (eu or com)
+     */
+    public function __construct($authtoken, $extra_auth_params = array(), $auth_url="https://accounts.zoho.com/login",$domain = 'https://crm.zoho.com') {
+        $this->authtoken = $authtoken;
 
         $this->ticket = null;
+
+        $this->domain = $domain;
     }
 
-    /** 
-    * Retrieves a single record from a specified Zoho CRM module based on a record ID
-    * @param  $module  The module from which the record will be retrieved
-    * @param  $record_id  The ID of the record that will be retrieved
-    */
+    /**
+     * Retrieves a single record from a specified Zoho CRM module based on a record ID
+     *
+     *
+     * @param  $module  array The module from which the record will be retrieved
+     * @param  $record_id  int The ID of the record that will be retrieved
+     * @return array
+     */
     public function get_module_data_by_id($module, $record_id) {
 
         $post = array(
@@ -34,18 +59,21 @@ class Zoho {
         );
 
         $q = http_build_query($post);
-        
-        $response = $this->openUrl("https://crm.zoho.com/crm/private/json/$module/getRecordById",$q);
-	
+
+        $response = $this->openUrl("{$this->domain}/crm/private/json/$module/getRecordById",$q);
+
         return $this->json_to_array(json_decode($response), $module);
 
     }
 
-    /** 
-    * Retrieves a single record from a specified Zoho CRM module based on some search criteria
-    * @param  $module  The module from which the record will be retrieved
-    * @param  $search_criteria  The criteria used to search for the record
-    */
+    /**
+     * Retrieves a single record from a specified Zoho CRM module based on some search criteria
+     * @param  $module  array The module from which the record will be retrieved
+     * @param  $search_criteria int The criteria used to search for the record
+     *
+     *
+     * @return array
+     */
     public function get_record_by_searching($module, $search_criteria) {
         $post = array(
             'newFormat' => 1,
@@ -57,33 +85,38 @@ class Zoho {
         );
 
         $q = http_build_query($post);
-        
-        $response = $this->openUrl("https://crm.zoho.com/crm/private/json/$module/searchRecords",$q);
-	
+
+        $response = $this->openUrl("{$this->domain}/crm/private/json/$module/searchRecords",$q);
+
         return $this->json_to_array(json_decode($response), $module);
 
     }
-    
+
     public function json_to_array($response_data, $module){
-    	$array_contents = array();
-    	foreach($response_data->response->result->$module->row->FL as $key => $record){
-		$array_contents[$record->val] = $record->content;
-	}
-	return $array_contents;
+        $array_contents = array();
+        foreach($response_data->response->result->$module->row->FL as $key => $record){
+            $array_contents[$record->val] = $record->content;
+        }
+        return $array_contents;
     }
-    
+
     /**
-    *  Updates a specified record in Zoho CRM 
-    *  @param  $module  The module in which the record resides
-    *  @param  $record_ID  The ID of the record to be updated
-    *  @param  $update_data  The new data for the record
-    * https://crm.zoho.com/crm/private/xml/Module/updateRecords?newFormat=1&apikey=APIkey&ticket=Ticket
-    **/
+     *  Updates a specified record in Zoho CRM
+     *  @param  $module  array The module in which the record resides
+     *  @param  $record_ID  int The ID of the record to be updated
+     *  @param  $update_data  string The new data for the record
+     *  @param array $extra_post_parameters
+     *
+     * https://crm.zoho.com/crm/private/xml/Module/updateRecords?newFormat=1&apikey=APIkey&ticket=Ticket
+     *
+     * @return bool
+     * @throws ZohoException
+     */
     public function update_record($module, $record_ID, $update_data, $extra_post_parameters=array()) {
         $xmldata = $this->XMLfy($update_data, $module);
         $post = array(
             'newFormat' => 1,
-	    'authtoken' => $this->authtoken,
+            'authtoken' => $this->authtoken,
             'version' => 2,
             'xmlData' => $xmldata,
             'duplicateCheck' => 2,
@@ -96,7 +129,7 @@ class Zoho {
         $q = http_build_query($post);
 
 
-        $response = $this->openUrl("https://crm.zoho.com/crm/private/xml/$module/updateRecords", $q);
+        $response = $this->openUrl("{$this->domain}/crm/private/xml/$module/updateRecords", $q);
 
         $this->check_successful_xml($response);
 
@@ -105,16 +138,21 @@ class Zoho {
     }
 
     /**
-    *  Adds a new record in Zoho CRM 
-    *  @param  $module  The module in which the record will reside
-    *  @param  $update_data  The new data for the record
-    * https://crm.zoho.com/crm/private/xml/Module/updateRecords?newFormat=1&apikey=APIkey&ticket=Ticket
-    **/
+     *  Adds a new record in Zoho CRM
+     *  @param  $module  array The module in which the record will reside
+     *  @param  $update_data  array The new data for the record
+     *  @param  $extra_post_parameters  array Some POST Parameters
+     *
+     * @return mixed|null
+     * @throws ZohoException
+     *
+     * https://crm.zoho.com/crm/private/xml/Module/updateRecords?newFormat=1&apikey=APIkey&ticket=Ticket
+     **/
     public function insert_record($module, $update_data, $extra_post_parameters=array()) {
         $xmldata = $this->XMLfy($update_data, $module);
         $post = array(
             'newFormat' => 1,
-	       'authtoken' => $this->authtoken,
+            'authtoken' => $this->authtoken,
             'version' => 2,
             'xmlData' => $xmldata,
             'duplicateCheck' => 2,
@@ -126,23 +164,31 @@ class Zoho {
         $q = http_build_query($post);
 
 
-        $response = $this->openUrl("https://crm.zoho.com/crm/private/xml/$module/insertRecords", $q);
+        $response = $this->openUrl("{$this->domain}/crm/private/xml/$module/insertRecords", $q);
 
         $this->check_successful_xml($response);
 
         return $response;
 
     }
-    
+
+
     /**
-    * https://crm.zoho.com/crm/private/xml/Leads/convertLead?newFormat=1&apikey=APIkey&ticket=Ticket
-    **/
+     * https://crm.zoho.com/crm/private/xml/Leads/convertLead?newFormat=1&apikey=APIkey&ticket=Ticket
+     *
+     *
+     * @param $lead_ID
+     * @param $potential_data
+     * @param array $extra_post_parameters
+     * @return mixed
+     * @throws ZohoException
+     */
     public function convert_lead($lead_ID, $potential_data, $extra_post_parameters=array()) {
         $this->ensure_opened();
         $xmldata = $this->XMLfy($potential_data, "Potentials");
         $post = array(
             'newFormat' => 1,
-	    'authtoken' => $this->authtoken,
+            'authtoken' => $this->authtoken,
             'version' => 2,
             'xmlData' => $xmldata,
             'duplicateCheck' => 2,
@@ -154,42 +200,56 @@ class Zoho {
 
         $q = http_build_query($post);
 
-        $response = openUrl("https://crm.zoho.com/crm/private/xml/Leads/convertLead", $q);
+        $response = $this->openUrl("{$this->domain}/crm/private/xml/Leads/convertLead", $q);
 
         $this->check_successful_xml($response);
 
         return $response;
 
     }
-    
+
     /**
-    * Uses curl to open a URL
-    */
-    function openUrl($url, $data=null) {
-	    $ch = curl_init();
-	    $timeout = 5;
-	
-	    if($data) {
-	        curl_setopt($ch,CURLOPT_POST,1);
-	        curl_setopt($ch,CURLOPT_POSTFIELDS, $data);
-	        curl_setopt($ch,CURLOPT_VERBOSE, true);
-	        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-	        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-	
-	   }
-	
-	    curl_setopt($ch,CURLOPT_URL,$url);
-	    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-	    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
-	    $data = curl_exec($ch);
-	    curl_close($ch);
-	    return $data;
+     * Uses curl to open a URL
+     *
+     * @param $url
+     * @param null $data
+     * @return mixed|null
+     */
+    public function openUrl($url, $data=null) {
+        $ch = curl_init();
+        $timeout = 5;
+
+        if($data) {
+            curl_setopt($ch,CURLOPT_POST,1);
+            curl_setopt($ch,CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch,CURLOPT_VERBOSE, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        }
+
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
     }
-    
+
+
+    /**
+     *
+     * Check if a XML is correct
+     *
+     * @param $response
+     * @return bool
+     * @throws ZohoException
+     */
     public function check_successful_xml($response) {
         $html = new DOMDocument();
         $html->loadXML($response);
 
+        /** @var DOMDocument $err */
         if ($err = $html->getElementsByTagName('error')->item(0)) {
             throw new ZohoException($err->getElementsByTagName('message')->item(0)->nodeValue);
         }
@@ -197,6 +257,14 @@ class Zoho {
         return true;
     }
 
+
+
+
+    /**
+     * @param $arr
+     * @param $openingBracket
+     * @return string
+     */
     public function XMLfy ($arr, $openingBracket) {
         $xml = "<$openingBracket>";
         $no = 1;
@@ -209,7 +277,7 @@ class Zoho {
             $no += 1;
         }
         $xml .= "</$openingBracket>";
-        
+
         return $xml;
     }
 
